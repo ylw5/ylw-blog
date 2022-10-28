428ca6ead3d422416b42f634deb73a6dd3f3191c
ylw5 <767138650@qq.com>
2022-10-28 18:25:30 +0800

    feat: add logo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,13 +3,22 @@ export default defineConfig({
   title: 'ylw-blog',
   titleTemplate: false,
   themeConfig: {
+    logo: {
+      src: '/logo.svg',
+      alt: 'ylw-blog',
+    },
+    siteTitle: false,
     nav: [
       { text: 'posts', link: '/posts/' },
+      { text: 'project', link: '/' },
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/ylw5' },
       { icon: 'twitter', link: 'https://twitter.com/ZhenMorant' },
     ],
+    footer: {
+      copyright: '© 2022-present Lingwei Ye',
+    },
   },
   lastUpdated: true,
   cleanUrls: 'with-subfolders',
