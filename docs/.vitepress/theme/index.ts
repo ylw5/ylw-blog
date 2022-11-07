41a98ebd5d174e5e7aa8f4e1e605bb834b0ede8c
ylw5 <767138650@qq.com>
2022-11-07 18:23:07 +0800

    feature: polish aside style

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,31 @@
 import DefaultTheme from 'vitepress/theme'
 import './override.css'
-export default DefaultTheme
+import { nextTick, watch } from 'vue'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app, router, siteData }) {
+    const changeAsideOpacity = function () {
+      const aside = document.querySelector('.aside') as HTMLElement
+      if (document.documentElement.scrollTop !== 0)
+        aside.classList.add('disappear')
+      else
+        aside.classList.remove('disappear')
+    }
+
+    watch(router.route, () => {
+      const path = router.route.path
+      let hasScroll = false
+      if (path.startsWith('/posts/') && path !== '/posts/') {
+        nextTick(() => {
+          document.addEventListener('scroll', changeAsideOpacity)
+          hasScroll = true
+        })
+      }
+      else if (hasScroll) {
+        document.removeEventListener('scroll', changeAsideOpacity)
+      }
+    })
+  },
+
+}
